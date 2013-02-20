04c14de0d0035bb3bcfedbc33c6703901fe16386
Frédéric DE MATOS <frederic.dematos@gmail.com>
2013-02-20 21:11:17 +0100

    Initial Commit

---
+++ SmartLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartLayout
+//
+//  Created by Frédéric DE MATOS on 20/02/13.
+//  Copyright (c) 2013 smartfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartLayout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartLayout
+//
+//  Created by Frédéric DE MATOS on 20/02/13.
+//  Copyright (c) 2013 smartfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
