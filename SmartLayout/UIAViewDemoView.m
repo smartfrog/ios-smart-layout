8484770d38da413f9a9aedf97f23bbb7e6247538
Frédéric DE MATOS <frederic.dematos@gmail.com>
2013-02-22 00:00:55 +0100

    Enhancement: use addSubView instead of addSubView withSize for button2 in UIVView demo

--- SmartLayout/UIAViewDemoView.m
+++ SmartLayout/UIAViewDemoView.m
@@ -18,23 +18,19 @@
         [self setBackgroundImage:@"background"]; // You can also use initWithBackgroundImage
         
 
-        
         UIButton * button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        [button1 setTitle:@"Button1" forState:UIControlStateNormal];
-        
+        [button1 setTitle:@"Button1" forState:UIControlStateNormal];        
         [self addSubview:button1 toTop:@20 right:@20 bottom:@20 left:@20];
         
         UIButton * button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         button2.frame = CGRectMake(0, 0, 200, 200);
         [button2 setTitle:@"Button2" forState:UIControlStateNormal];
-        
         [self addSubview:button2 toTop:@30 right:nil bottom:nil left:@30];
         
         
         UIButton * button3 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [button3 setTitle:@"Button3" forState:UIControlStateNormal];
         button3.frame = CGRectMake(0, 0, 200, 200);
-        
         [self addSubview:button3 toTop:nil right:@30 bottom:@30 left:nil];
         
         
--- SmartLayout/UIVViewDemoView.m
+++ SmartLayout/UIVViewDemoView.m
@@ -15,10 +15,7 @@
     self = [super initWithFrame:CGRectMake(0, 0, 1024, 699)];
     if (self)
     {
-        
-        
         [self setBackgroundImage:@"background"]; // You can also  use initWithBackgroundImage
-        
         
         self.padding = 20;
         self.gap = 20;
@@ -27,18 +24,15 @@
         
         UIButton * button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [button1 setTitle:@"Button1" forState:UIControlStateNormal];
-        
         [self addSubview:button1 withSize:CGSizeMake(200, 200)];
         
         UIButton * button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         button2.frame = CGRectMake(0, 0, 200, 200);
         [button2 setTitle:@"Button2" forState:UIControlStateNormal];
-        
-        [self addSubview:button2 withSize:CGSizeMake(200, 200)];
+        [self addSubview:button2];
         
         UIButton * button3 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         [button3 setTitle:@"Button3" forState:UIControlStateNormal];
-        
         [self addSubview:button3 withSize:CGSizeMake(200, 200)];
         
         
